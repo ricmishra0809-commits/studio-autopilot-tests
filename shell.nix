f72b91926da7f67cbfbb45a7c21d8635d1300387
Rishiraj Mishra <ricmishra0809@gmail.com>
2025-09-19 14:27:39 +0000

    nix-shell studio-8483198719:~/studio{main}$ nix-shell error:        … wh

--- shell.nix
+++ shell.nix
@@ -1,4 +1,3 @@
-
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
 let
   nodejs = pkgs.nodejs_20;
@@ -7,62 +6,35 @@ in
 pkgs.mkShell {
   name = "firebase-studio";
   buildInputs = with pkgs; [
-    # Node.js and package managers
     nodejs
     pnpm
-
-    # Firebase
-    google-cloud-sdk
-    
     # Playwright dependencies
     chromium
     firefox
-    webkitgtk
-    
-    # Additional libraries needed by Playwright
+    webkitgtk_6_0
+    # System dependencies for Playwright
     glib
     nss
     nspr
-    cups
+    dbus
+    libatk-1_0
+    at-spi2-core
+    libxkbcommon
+    libX11
+    libXcomposite
+    libXdamage
+    libXext
+    libXfixes
+    libXrandr
     libdrm
     libgbm
-    libxkbcommon
-    at-spi2-atk
-    libxshmfence
-    libepoxy
-    libjpeg
-    ffmpeg
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libxcb
-    xorg.libxkbfile
-    xorg.libxmu
-    xorg.xauth
-    alsa-lib
-    expat
-    libdbusmenu
-    gdk-pixbuf
-    cairo
-    pango
-    gtk3
     udev
+    alsa-lib
+    pango
+    cairo
+    harfbuzz
   ];
-
   shellHook = ''
-    # Set the path for Playwright to find its browser binaries
-    export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright.browsers}/
-    # Set NPM config
-    # this will create a .npmrc file in the project directory
-    npm config set fund false
-    npm config set audit false
-    echo "Nix-shell environment is ready."
+    export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}/
   '';
 }
