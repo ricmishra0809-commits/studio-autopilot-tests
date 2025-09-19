6caa2b5303d9bdbd4893f9eddefb592174ddb5ea
Rishiraj Mishra <ricmishra0809@gmail.com>
2025-09-19 14:17:33 +0000

    studio-8483198719:~/studio{main}$ nix-shell error:        … while callin

--- shell.nix
+++ shell.nix
@@ -1,46 +1,45 @@
-{ pkgs ? import <nixpkgs> { } }:
-let
-  nodejs = pkgs.nodejs-20_x;
-  pnpm = pkgs.nodePackages.pnpm;
-  firebase-tools = pkgs.nodePackages.firebase-tools;
 
-  playwright-deps = with pkgs; [
-    # Deps from https://playwright.dev/docs/ci#nix
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/g8zzlf6drg73c987ii390yicq4c0j778.zip") {} }:
+let
+  nodejs = pkgs.nodejs-18_x;
+  pnpm = pkgs.nodePackages.pnpm;
+in pkgs.mkShell {
+  name = "studio-shell";
+  buildInputs = with pkgs; [
+    nodejs
+    pnpm
+    # Additional dependencies for Playwright
+    glib
+    nspr
+    nss
+    dbus
+    libatk
+    atk-bridge
+    expat
+    at-spi2-atk
+    libxkbcommon
     xorg.libX11
     xorg.libXcomposite
     xorg.libXdamage
     xorg.libXext
     xorg.libXfixes
     xorg.libXrandr
-    xorg.libXtst
-    xorg.libxkbcommon
     xorg.libxcb
-    nss
-    nspr
+    udev
     alsa-lib
-    at-spi2-atk
-    cups
-    libexpat
-    libuuid
+    pango
+    cairo
+    harfbuzz
     libdrm
     libgbm
-    libxkbcommon
-    mesa
-    pango
-    pipewire
-    udev
   ];
-
-in
-  pkgs.mkShell {
-    buildInputs = [
-      nodejs
-      pnpm
-      firebase-tools
-      pkgs.google-cloud-sdk
-    ] ++ playwright-deps;
-
-    shellHook = ''
-      export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright.browsers-json}/.
-    '';
-  }
+  shellHook = ''
+    # Set NPM config to use the Nix store
+    export NPM_CONFIG_PREFIX=$(pwd)/.npm-packages
+    export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
+    # Set Node-specific environment variables
+    export NODE_PATH=$NODE_PATH:$(nix-build --no-out-link "<nixpkgs>" -A nodejs.pkgs.node_modules)/lib/node_modules
+    echo "Nix-shell environment for Studio AutoPilot is ready."
+    echo "Run 'npm run dev' and 'npm run genkit:watch' in separate terminals."
+  '';
+}
